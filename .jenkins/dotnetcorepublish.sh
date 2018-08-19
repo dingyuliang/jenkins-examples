4af3445da0c3e950f1a73e62b78485b8ca403f58
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 12:38:51 -0700

    Update and rename dotnetcorepublish.sh to dotnetcorepublish.bat

--- .jenkins/dotnetcorepublish.sh
+++
@@ -1,4 +0,0 @@
-dotnet publish ../src/DotNetCore/DotNetCore.Jenkins/DotNetCore.Jenkins.csproj -o bin/publish
-rmdir /s /q "E:/websites/deployed/dotnetcore"
-mkdir "E:/websites/deployed/dotnetcore"
-xcopy "../src/DotNetCore/DotNetCore.Jenkins/bin/publish" "E:/websites/deployed/dotnetcore" /s /e
