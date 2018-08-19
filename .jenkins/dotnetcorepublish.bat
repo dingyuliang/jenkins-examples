7a1299c773354d859c91579ec284618e0aba851c
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 12:43:35 -0700

    Update dotnetcorepublish.bat

--- .jenkins/dotnetcorepublish.bat
+++ .jenkins/dotnetcorepublish.bat
@@ -1,4 +1,3 @@
 dotnet publish src/DotNetCore/DotNetCore.Jenkins/DotNetCore.Jenkins.csproj -o bin/publish
-rmdir /s /q "E:/websites/deployed/dotnetcore"
-mkdir "E:/websites/deployed/dotnetcore"
-xcopy "src/DotNetCore/DotNetCore.Jenkins/bin/publish" "E:/websites/deployed/dotnetcore" /s /e
+rmdir /s /q %1
+xcopy "src/DotNetCore/DotNetCore.Jenkins/bin/publish" %1 /s /e
