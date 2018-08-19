a93b65a80881f8ffb04734f53632ba85b5895f7e
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 13:08:06 -0700

    Update dotnetcorepublish.bat

--- .jenkins/dotnetcorepublish.bat
+++ .jenkins/dotnetcorepublish.bat
@@ -1,3 +1,5 @@
-dotnet publish src/DotNetCore/DotNetCore.Jenkins/DotNetCore.Jenkins.csproj -o bin/publish
+dotnet publish src/DotNetCore/DotNetCore.Jenkins/DotNetCore.Jenkins.csproj -o bin/publish -c Release
+"C:\\Windows\\System32\\inetsrv\\appcmd.exe" stop apppool /apppool.name:"Services"
 rmdir /s /q %1
 xcopy "src/DotNetCore/DotNetCore.Jenkins/bin/publish" %1 /s /e
+"C:\\Windows\\System32\\inetsrv\\appcmd.exe" start apppool /apppool.name:"Services"
