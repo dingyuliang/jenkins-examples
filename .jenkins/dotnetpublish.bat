d8284ff10a63c2164e9f1f0f4a05b3b6e950e6a3
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 15:52:52 -0700

    Update dotnetpublish.bat

--- .jenkins/dotnetpublish.bat
+++ .jenkins/dotnetpublish.bat
@@ -1,8 +1,7 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Configuration=Release /p:PublishProfile=FolderProfile
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj "/p:Platform=AnyCPU;Configuration=Release;PublishDestination=E:/published" /t:PublishToFileSystem
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Platform=AnyCPU /p:Configuration=Release /p:PublishDestination=E:/published /t:PublishToFileSystem
 
 "C:\\Windows\\System32\\inetsrv\\appcmd.exe" stop apppool /apppool.name:"%2"
-if exist %1
-   rmdir /s /q %1
+if exist %1 (rmdir /s /q %1)
 echo d | xcopy "src/DotNet/DotNet.Jenkins/bin/release/publish" %1 /s /e
 "C:\\Windows\\System32\\inetsrv\\appcmd.exe" start apppool /apppool.name:"%2"
