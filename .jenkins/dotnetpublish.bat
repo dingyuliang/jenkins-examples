e141a98d323d6d07d4ab12e94f6a89c2da5cca0e
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 15:48:58 -0700

    Update dotnetpublish.bat

--- .jenkins/dotnetpublish.bat
+++ .jenkins/dotnetpublish.bat
@@ -1,4 +1,6 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Configuration=Release /p:PublishProfile=FolderProfile
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj "/p:Platform=AnyCPU;Configuration=Release;PublishDestination=E:/published" /t:PublishToFileSystem
+
 "C:\\Windows\\System32\\inetsrv\\appcmd.exe" stop apppool /apppool.name:"%2"
 if exist %1
    rmdir /s /q %1
