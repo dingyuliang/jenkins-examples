f1fe81a0da73e3267dc3d2240389c8acd3d0feb4
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 11:54:44 -0700

    Update dotnetcorepublish.sh

--- .jenkins/dotnetcorepublish.sh
+++ .jenkins/dotnetcorepublish.sh
@@ -1,1 +1,4 @@
-dotnet publish 
+dotnet publish ../src/DotNetCore/DotNetCore.Jenkins/DotNetCore.Jenkins.csproj -o bin/publish
+rmdir /s /q "E:/websites/deployed/dotnetcore"
+mkdir "E:/websites/deployed/dotnetcore"
+xcopy "../src/DotNetCore/DotNetCore.Jenkins/bin/publish" "E:/websites/deployed/dotnetcore" /s /e
