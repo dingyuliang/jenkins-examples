838287077f429f61e21fd7d055940b03187a1e84
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 16:04:34 -0700

    Update dotnetpublish.bat

--- .jenkins/dotnetpublish.bat
+++ .jenkins/dotnetpublish.bat
@@ -1,5 +1,7 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Configuration=Release /p:PublishProfile=FolderProfile
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Platform=AnyCPU /p:Configuration=Release /p:PublishDestination=E:/published /t:PublishToFileSystem
+
+rem doesnt work
+rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" src/DotNet/DotNet.Jenkins/DotNet.Jenkins.csproj /p:DeployOnBuild=true /p:Platform=AnyCPU /p:Configuration=Release /p:PublishDestination=E:/published /t:PublishToFileSystem
 
 ("C:\\Windows\\System32\\inetsrv\\appcmd.exe" list apppool /apppool.name:"%2") | find /i "state:Started"
 if not errorlevel 1 ("C:\\Windows\\System32\\inetsrv\\appcmd.exe" stop apppool /apppool.name:"%2")
