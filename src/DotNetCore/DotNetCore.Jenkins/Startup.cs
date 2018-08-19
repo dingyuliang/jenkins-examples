76b8428429814188692327f92177ea09d6f7fc54
Yuliang Ding <dingyuliang@msn.com>
2018-08-19 13:07:22 -0700

    Removed HTTPS

--- src/DotNetCore/DotNetCore.Jenkins/Startup.cs
+++ src/DotNetCore/DotNetCore.Jenkins/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,7 +41,11 @@ namespace DotNetCore.Jenkins
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.Run(async (context) => {
+            //    await context.Response.WriteAsync("Welcome!");  
+            //});
+
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
